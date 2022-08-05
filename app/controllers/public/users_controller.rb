cadc705eedff1b3039981a40cd34e05ab1d1fc15
dmmwebcampaizawa <105425062+dmmwebcampaizawa@users.noreply.github.com>
2022-08-05 12:41:35 +0000

    TOP、userのview作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,5 +3,28 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to my_page_path
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+
 end
