6fa6abcd354f8c6cfb532e31eb25f715be7f51a7
dmmwebcampaizawa <105425062+dmmwebcampaizawa@users.noreply.github.com>
2022-08-05 02:11:07 +0000

    環境構築

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.active_job.queue_adapter = :inline
+
+  config.hosts << "191ff53530c6421d9b0bcaefd1e3869e.vfs.cloud9.us-east-1.amazonaws.com"
 end
