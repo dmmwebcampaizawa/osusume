5dfaac74745e000846afe6e85f28e3ee64493f62
dmmwebcampaizawa <105425062+dmmwebcampaizawa@users.noreply.github.com>
2022-08-23 06:45:44 +0000

    path修正

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -2,6 +2,6 @@ class Public::TagsController < ApplicationController
   def destroy
     @tag_lists = Tag.find(params[:id])
     @tag_lists.destroy
-    redirect_back(fallback_location: root_path)
+    redirect_to admin_tag_path
   end
 end
